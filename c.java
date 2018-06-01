7563ab6de698eeb1de57b88327d16e1ca3d6deb6
sutapa4612 <39484717+sutapa4612@users.noreply.github.com>
2018-06-01 03:16:28 -0700

    Add files via upload

---
+++ c.java
@@ -0,0 +1,15 @@
+package student;
+public class c
+{
+String name,department;
+public c(String n,String d)
+{
+name=n;
+department=d;
+}
+public void display()
+{
+System.out.println("name" +name);
+System.out.println("department" +department);
+}
+}
---
+++ pack.java
@@ -0,0 +1,9 @@
+import student.*;
+class pack
+{
+public static void main(String args[])
+{
+c ob=new c(" Sutapa"," ECE");
+ob.display();
+}
+}
